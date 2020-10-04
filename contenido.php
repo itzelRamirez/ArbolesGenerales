4751a2239ed6f4c5957396d82ad5fe6f814847fb
itzelRamirez <51004731+itzelRamirez@users.noreply.github.com>
2020-10-04 14:59:59 -0500

    Add files via upload

---
+++ contenido.php
@@ -0,0 +1,28 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/imagen1.jpg"."||".
+				  "Margaritas"."||".
+				  "Bellis perenis, comunmente llamada chiribita, margarita comun, pascueta o vellorita es una planta herbacea perteneciente a la familia de las Asteraceas muy utilizada a efectos decoorativoos mezcla con el cesped, por sus colores y su resistencia a la siega.";
+
+		$datos[1]="img/imagen2.jpg"."||".
+				  "Azucena"."||".
+				  "Lilium candidum, lirio o azucena, del arabe assusana y este del hebreo es una plata vivaz de porte herbaceo, que pertenece a la familia de la liliaceas.";
+
+		$datos[2]="img/imagen3.jpg"."||".
+				  "Amaryllis"."||".
+				  "Es un genero de plantas herbaceas, perennes y bulbosas pertenecientes a la familia de las amarillidaceas. Este genero, oriundo del sur de Africa, fue descrito por Carlos Linneo en 1753.";
+				  
+		$datos[3]="img/imagen4.jpg"."||".
+				  "Lisianthus"."||".
+			      "Es muy similar a las rosas y a las peonias, y se caracteriza por tener un conjunto de petalos abultado, denominado tambien corola, lo que hace sumamente atractivas y llamativas para decorar.";
+
+		$datos[4]="img/imagen5.jpg"."||".
+				  "Disocactus"."||".
+				  "Es una especie de planta fanerogama de la familia Cactaceae. Vulgarmente se llama cactus cola de rata.";
+		return $datos;
+	}
+
+ ?>
